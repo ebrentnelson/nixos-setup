46ad8a8327627febff2c44519e1cd60be56ff65a
E Brent Nelson <ebrentnelson@gmail.com>
2025-07-28 15:24:19 -0600

    Add disko.nix

---
+++ disko.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, ... }:
+
+let
+  disk = "/dev/sda"; # Replace with your disk
+in
+{
+  disko.devices = {
+    disk.main = {
+      device = disk;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "efi";
+            bootable = true;
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot/efi";
+            };
+          };
+          swap = {
+            size = "8G";
+            content = {
+              type = "swap";
+              resumeDevice = true;
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "btrfs";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+  };
+}
