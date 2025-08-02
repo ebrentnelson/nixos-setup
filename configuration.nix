0bf94f225a9823870ce023048f4f8baab8e76c21
E Brent Nelson <ebrentnelson@gmail.com>
2025-08-02 01:01:07 -0600

    Retry

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,10 @@
 { config, pkgs, ... }:
 
 {
-  # imports =
-  #   [ # Include the results of the hardware scan.
-  #     ./hardware-configuration.nix
-  #   ];
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
   # Network configuration
   networking.hostName = "moroni";
--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS configuration with Home Manager and Disko";
+  description = "NixOS configuration with Home Manager";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
@@ -7,57 +7,15 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    disko = {
-      url = "github:nix-community/disko/latest";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { nixpkgs, home-manager, disko, ... }:
+  outputs = { nixpkgs, home-manager, ... }:
   let
     hostname = "moroni";  # Change this one line to rename the host
   in {
     nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        disko.nixosModules.disko
-        {
-          disko.devices = {
-            disk.main = {
-              device = "/dev/disk/by-id/some-disk-id";
-              type = "disk";
-              content = {
-                type = "gpt";
-                partitions = {
-                  ESP = {
-                    size = "512M";
-                    type = "EF00";
-                    content = {
-                      type = "filesystem";
-                      format = "vfat";
-                      mountpoint = "/boot";
-                    };
-                  };
-                  swap = {
-                    size = "8G";
-                    content = {
-                      type = "swap";
-                      resumeDevice = true;
-                    };
-                  };
-                  root = {
-                    size = "100%";
-                    content = {
-                      type = "filesystem";
-                      format = "ext4";
-                      mountpoint = "/";
-                    };
-                  };
-                };
-              };
-            };
-          };
-        }
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
