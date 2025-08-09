4d0d2a9f7ebb6d00c38c081474dea401401b98ad
E Brent Nelson <ebrentnelson@gmail.com>
2025-08-09 03:17:34 -0600

    Done with polybar

--- users/ebn/modules/polybar.nix
+++ users/ebn/modules/polybar.nix
@@ -1,53 +1,67 @@
 { config, pkgs, ... }:
 
 {
- services.polybar = {
-   enable = true;
-   script = "polybar main &";
-   config = {
-     "bar/main" = {
-       width = "100%";
-       height = 50;
-       background = "#33ccff";
-       foreground = "#1e1e2e";
-       
-       modules-left = "xworkspaces";
-       modules-center = "date";
-       modules-right = "pulseaudio tray powermenu";
-       
-       font-0 = "GeistMono Nerd Font:size=13;2";
-       
-       tray-position = "right";
-       tray-spacing = 8;
-       tray-padding = 4;
-     };
-     
-     "module/xworkspaces" = {
-       type = "internal/xworkspaces";
-       label-active = "●";
-       label-active-padding = 1;
-       label-occupied = "○";
-       label-occupied-padding = 1;
-       label-empty = "";
-     };
-     
-     "module/date" = {
-       type = "internal/date";
-       date = "%H:%M";
-     };
-     
-     "module/pulseaudio" = {
-       type = "internal/pulseaudio";
-       format-volume = "🔊 <label-volume>";
-       format-muted = "🔇 Muted";
-       click-right = "pavucontrol";
-     };
-     
-     "module/powermenu" = {
-       type = "custom/text";
-       content = " ⏻ ";
-       click-left = "rofi -show p -modi p:rofi-power-menu";
-     };
-   };
- };
+  services.polybar = {
+    enable = true;
+    script = "polybar main &";
+    config = {
+      "bar/main" = {
+        width = "100%";
+        height = 50;
+        background = "#1ae5cc";
+        foreground = "#1e1e2e";
+
+        modules-left = "xworkspaces";
+        modules-center = "date";
+        modules-right = "network alsa powermenu";
+
+        font-0 = "GeistMono Nerd Font:size=13;2";
+
+        tray-position = "right";
+        tray-spacing = 8;
+        tray-padding = 4;
+
+        module-margin-left = 1;  
+        module-margin-right = 1;
+      };
+
+      "module/xworkspaces" = {
+        type = "internal/xworkspaces";
+        label-active = "●";
+        label-active-padding = 1;
+        label-occupied = "○";
+        label-occupied-padding = 1;
+        label-empty = "";
+      };
+
+      "module/date" = {
+        type = "internal/date";
+        date = "%H:%M";
+      };
+
+      "module/network" = {
+        type = "internal/network";
+        interface-type = "wireless";
+        format-connected = " 󰤨 <label-connected> ";
+        format-disconnected = " 󰤭 Disconnected ";
+        label-connected = "%signal%%";
+      };
+
+      "module/alsa" = {
+        type = "internal/alsa";
+        format-volume = " <ramp-volume> <label-volume> ";
+        format-muted = " 󰝟 Muted ";
+        ramp-volume-0 = "󰕿";
+        ramp-volume-1 = "󰖀";
+        ramp-volume-2 = "󰕾";
+        click-right = "pavucontrol";
+      };
+
+      "module/powermenu" = {
+        type = "custom/text";
+        content = " 󰐥 ";
+        click-left = "rofi -show p -modi p:rofi-power-menu";
+      };
+    };
+  };
 }
